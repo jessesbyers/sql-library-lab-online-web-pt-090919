5d9a4ca309dabc147eeca9fda9a59f1a5eed1d8b
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-06 17:51:09 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,7 @@ def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto
   FROM characters
 
-  ORDER BY characters.motto DESC
+  ORDER BY characters.motto ASC
 
   "
   # LIMIT 1
