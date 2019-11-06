9213509497388428f3463671681e94ff36f5a55b
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-06 18:24:23 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -52,7 +52,7 @@ def select_series_title_with_most_human_characters
   JOIN characters
   ON character_books.character_id = characters.id
   WHERE characters.species = 'human'
-  ORDER BY COUNT ASC
+  ORDER BY ASC
 
 
   "
