c60b866bac9f3307971da5b6af80d46d6dd5d539
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-06 18:01:12 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,10 +37,10 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
   JOIN series
-  ON authors.id = series.author_id"
+  ON authors.id = series.author_id
   FROM series
   JOIN subgenres
-  ON series.subgenre_id = subgenres.id
+  ON series.subgenre_id = subgenres.id"
 end
 # WHERE
 # GROUP BY
