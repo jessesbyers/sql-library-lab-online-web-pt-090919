8f5dfb96ef3f56e4b173703bb51fcae63797d826
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-06 17:21:55 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  
+);
+
+CREATE TABLE sub_genres (
+  id INTEGER PRIMARY KEY,
+  
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  
+);
+
+
+
+
