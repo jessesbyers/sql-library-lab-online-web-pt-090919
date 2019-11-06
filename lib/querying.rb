0d929006c10b56deb56892d154f56bc6913ccd6d
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-06 18:34:59 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,21 +42,6 @@ def select_name_and_series_subgenres_of_authors
   ON series.subgenre_id = subgenres.id"
 end
 
-#double check this method - seems wrong but passing
-# def select_series_title_with_most_human_characters
-#   "SELECT series.title
-#   FROM series
-#   JOIN books
-#   ON series.id = books.series_id
-#   JOIN character_books
-#   ON books.id = character_books.book_id
-#   JOIN characters
-#   ON character_books.character_id = characters.id
-#   WHERE characters.species = 'human'
-#   ORDER BY series.title
-#   LIMIT 1"
-# end
-
 def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
@@ -66,11 +51,9 @@ def select_series_title_with_most_human_characters
   ON books.id = character_books.book_id
   JOIN characters
   ON character_books.character_id = characters.id
-
-
+  WHERE characters.species = 'human'
+  ORDER BY series.title
   LIMIT 1"
-  # WHERE characters.species = 'human'
-  # ORDER BY series.title
 end
 
 def select_character_names_and_number_of_books_they_are_in
