ae45ac21cd8ebf522501c0f15b444f6fab94502f
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-06 18:25:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -53,6 +53,7 @@ def select_series_title_with_most_human_characters
   ON character_books.character_id = characters.id
   WHERE characters.species = 'human'
   ORDER BY series.title
+  LIMIT 1
 
 
   "
