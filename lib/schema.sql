95a0f391a267d76d6400b4b3c27a26138491b216
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-06 17:26:16 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,24 +1,31 @@
 CREATE TABLE series (
   id INTEGER PRIMARY KEY,
-
+  title TEXT,
+  author_id INTEGER,
+  sub_genre_id INTEGER
 );
 
 CREATE TABLE sub_genres (
   id INTEGER PRIMARY KEY,
-
+  name TEXT
 );
 
 CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
-
+  name TEXT
 );
 
 CREATE TABLE books (
   id INTEGER PRIMARY KEY,
-
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
 );
 
 CREATE TABLE characters (
   id INTEGER PRIMARY KEY,
-
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER
 );
